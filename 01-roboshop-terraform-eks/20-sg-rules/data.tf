0a391fcacdd8164001d5443f39c9ebe5849c6e1d
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-16 15:21:38 +0530

    eks node data

--- 01-roboshop-terraform-eks/20-sg-rules/data.tf
+++ 01-roboshop-terraform-eks/20-sg-rules/data.tf
@@ -29,3 +29,7 @@ data "aws_ssm_parameter" "openvpn_sg_id" {
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
   name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
 }
+
+data "aws_ssm_parameter" "eks_node_sg_id" {
+  name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
+}
